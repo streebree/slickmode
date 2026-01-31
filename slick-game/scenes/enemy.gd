415be9f189b4352d6586a783b2ab43550bb595e5
streebree <streebree@gmail.com>
2026-01-30 20:15:01 -0500

    Add dashing and killing enemies

---
+++ slick-game/scenes/enemy.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var damage_collision: CollisionShape2D = $Area2D/CollisionShape2D
+
+const SPEED = -10.0
+
+var is_dead = false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_dead and not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	velocity.x = SPEED
+	
+	if is_dead:
+		sprite.rotation_degrees += 500 * delta
+	
+	move_and_slide()
+
+func destroy(delta_x):
+	if delta_x > 0: 
+		velocity.x = 200
+	else:
+		velocity.x = -200
+	velocity.y = -150
+	is_dead = true
+	collision_shape.disabled = true
+	damage_collision.disabled = true
+
+	print("DO THING")
--- slick-game/scenes/snowman.gd
+++ slick-game/scenes/snowman.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
@@ -11,12 +12,35 @@ var damage_collision_count = 0
 
 var health = 3
 
-var delta_x = 0
+var delta_x_from_enemy_hit = 0
+
+var dash_duration = 0.5
+var dash_duration_current = 0.0
+
+var dash_cooldown = 0.5
+var dash_cooldown_current = 0.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if dash_cooldown_current > 0:
+		dash_cooldown_current -= delta
+		
+	if dash_duration_current > 0:
+		dash_duration_current -= delta
+		if dash_duration_current <= 0:
+			# dash just ended:
+			sprite.rotation_degrees = 0
+			dash_cooldown_current = dash_cooldown
+			print("END DASH")
+			
+	if Input.is_action_just_pressed("run") and dash_cooldown_current <= 0 and dash_duration_current <= 0:
+		# dash just started:
+		dash_duration_current = dash_duration
+		sprite.rotation_degrees = 30
+		print("START DASH")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -48,12 +72,13 @@ func _physics_process(delta: float) -> void:
 	if damage_collision_count > 0:
 		health -= 1
 		velocity.y -= 150
-		if delta_x > 0:
+		if delta_x_from_enemy_hit > 0:
 			velocity.x -= 200
-		if delta_x < 0:
+		if delta_x_from_enemy_hit < 0:
 			velocity.x += 200
 		damage_collision_count = 0
-		print("health", health)
+		# TODO: Handle health if you die.
+		print("health ", health)
 			
 	
 	move_and_slide()
@@ -69,9 +94,15 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 # Enemy collision
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
-	print("GET HIT")
-	delta_x = body.position.x - position.x
-	damage_collision_count += 1
+	# Only take damage if you're not dashing.
+	if dash_duration_current <= 0:
+		delta_x_from_enemy_hit = body.position.x - position.x
+		damage_collision_count += 1
+	else:
+		print("body.name", body.name)
+		body.destroy(body.position.x - position.x)
+		# If you kill an enemy while dashing, you're dash time gets reset so you can chain them.
+		dash_cooldown_current = dash_cooldown
 
 func _on_area_2d_2_body_exited(body: Node2D) -> void:
 	pass
