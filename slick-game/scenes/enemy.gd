707e847a30053f79efea3cb5c700bb703b9c3759
streebree <streebree@gmail.com>
2026-02-01 15:43:04 -0500

    Improve dashing

--- slick-game/scenes/enemy.gd
+++ slick-game/scenes/enemy.gd
@@ -5,8 +5,6 @@ extends CharacterBody2D
 @onready var damage_collision: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var character_body: CharacterBody2D = $"."
 
-const SPEED = -10.0
-
 var is_dead = false
 
 func _physics_process(delta: float) -> void:
@@ -14,22 +12,20 @@ func _physics_process(delta: float) -> void:
 	if not is_dead and not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	velocity.x = SPEED
-	
+	# When dying, they spin.
 	if is_dead:
 		sprite.rotation_degrees += 500 * delta
 	
 	move_and_slide()
 
 func destroy(delta_x):
+	# When dying, they fly up in the air and to the direction you hit them.
 	if delta_x > 0: 
-		velocity.x = 200
+		velocity.x = 400
 	else:
-		velocity.x = -200
+		velocity.x = -400
 	velocity.y = -150
 	is_dead = true
 	
 	# When dying, they don't collide with anything.
 	character_body.collision_mask = 0
-
-	print("DO THING")
--- slick-game/scenes/snowman.gd
+++ slick-game/scenes/snowman.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -350.0
 const SPEED_CAP = 150.0
+const EXTRA_DASH_SPEED = 150.0
 
 var ice_collision_count = 0
 var prev_x_velocity = 0
@@ -20,26 +21,41 @@ var dash_duration_current = 0.0
 var dash_cooldown = 0.5
 var dash_cooldown_current = 0.0
 
+var is_jumping_off_ice = false
+
+func start_dash(direction):
+	dash_duration_current = dash_duration
+	sprite.rotation_degrees = 30
+	if direction == 0:
+		if velocity.x > 0:
+			velocity.x = SPEED_CAP + EXTRA_DASH_SPEED
+		elif velocity.x < 0:
+			velocity.x = -SPEED_CAP - EXTRA_DASH_SPEED
+	else:
+		velocity.x = (SPEED_CAP + EXTRA_DASH_SPEED) * direction
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
+	# Decrease the dash cooldown.
 	if dash_cooldown_current > 0:
 		dash_cooldown_current -= delta
 		
+	# Decrease dash time.
 	if dash_duration_current > 0:
 		dash_duration_current -= delta
 		if dash_duration_current <= 0:
 			# dash just ended:
 			sprite.rotation_degrees = 0
 			dash_cooldown_current = dash_cooldown
-			print("END DASH")
-			
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping_off_ice = ice_collision_count > 0
 	# Letting go of jump makes you stop moving upward.
 	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
 		velocity.y /= 3.0
@@ -49,33 +65,27 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("run") and dash_cooldown_current <= 0 and dash_duration_current <= 0:
 		# dash just started:
-		dash_duration_current = dash_duration
-		sprite.rotation_degrees = 30
-		if direction == 0:
-			if velocity.x > 0:
-				velocity.x = SPEED_CAP
-			elif velocity.x < 0:
-				velocity.x = -SPEED_CAP
-		else:
-			velocity.x = SPEED_CAP * direction
-		print("START DASH")
-		
-	if is_on_wall():
+		start_dash(direction)
+	
+	# If you hit a wall and you're on ice or jumping from the ice, bounce off the wall to keep speed.
+	if is_on_wall() and (is_jumping_off_ice or ice_collision_count > 0):
 		velocity.x = -prev_x_velocity
-	if ice_collision_count == 0:
+	if ice_collision_count == 0 and (is_on_floor() or not is_jumping_off_ice):
 		if direction:
 			velocity.x += direction * SPEED * delta
 		elif is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, delta * SPEED)
 	
-	# Cap the speed
+	# If you're dashing, the speed cap is a little higher.
+	var current_speed_cap = SPEED_CAP + EXTRA_DASH_SPEED if dash_duration > 0 else SPEED_CAP
+	# Cap the speed, but change it slowly.
 	if absf(velocity.x) > SPEED_CAP:
 		if velocity.x > 0:
-			velocity.x = SPEED_CAP
+			velocity.x = move_toward(velocity.x, SPEED_CAP, delta * 300)
 		else:
-			velocity.x = -SPEED_CAP
+			velocity.x = move_toward(velocity.x, -SPEED_CAP, delta * 300)
 			
-	prev_x_velocity  = velocity.x
+	prev_x_velocity = velocity.x
 	
 	# Handle damage:
 	if damage_collision_count > 0:
@@ -108,12 +118,12 @@ func _on_area_2d_2_body_entered(body: Node2D) -> void:
 		delta_x_from_enemy_hit = body.position.x - position.x
 		damage_collision_count += 1
 	else:
-		# Else you're dashing:
-		print("body.name", body.name)
+		# Else you're dashing so destroy the enemy.
 		body.destroy(body.position.x - position.x)
 		# If you kill an enemy while dashing, you're dash time gets reset so you can chain them.
 		dash_duration_current = dash_duration
+		var direction := Input.get_axis("ui_left", "ui_right")
+		start_dash(direction)
 
 func _on_area_2d_2_body_exited(body: Node2D) -> void:
 	pass
-	# damage_collision_count -= 1
